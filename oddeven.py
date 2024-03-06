84fb60def05e68f6a716814a6171a0d831650036
khabbaz7878 <145169873+khabbaz7878@users.noreply.github.com>
2024-03-06 18:27:23 -0500

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,6 +1,12 @@
 num = int(input("Enter a number: "))
 
 if num % 2 == 0:
-    print(f"{num} is even")
+  print(f"{num} is even")
+
+if num % 5 == 0:
+  print(f"{num} is divisible by 5")
 else:
-    print(f"{num} is odd")
+  print(f"{num} is not divisible by 5")  
+
+if num % 2 != 0:
+  print(f"{num} is odd")
